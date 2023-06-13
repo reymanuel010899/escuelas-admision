e28432ded3ec19c5de36f819ec199bcec84b48ea
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-06-13 12:59:25 -0400

    version 2.0

--- inicio/context_proccessor.py
+++ inicio/context_proccessor.py
@@ -4,14 +4,14 @@ def context_user(request):
     if request.user.is_authenticated:
         user = request.user
         curso = EstudiantesModels.objects.filter(user=user).first()
-        #personales = DatosPersonales.objects.get(estudiante=curso)
-        #datos_militares = DatosSiEsMilitar.objects.get(estudiante=curso)
+        personales = DatosPersonales.objects.get(estudiante=curso)
+        datos_militares = DatosSiEsMilitar.objects.get(estudiante=curso)
     
         
         return {
             'escuela':curso,
-            #'personales':personales,
-            #'datosmilitares':datos_militares
+            'personales':personales,
+            'datosmilitares':datos_militares
         }
         
     else:
